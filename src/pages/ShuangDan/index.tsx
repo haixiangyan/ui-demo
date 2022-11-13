62a6c6a613594c7a6639dc3a24841edfbf6b10f9
haixiangyan <haixiang6123@gmail.com>
2022-11-13 10:41:03 +0800

    feat: 添加 Header 行为

--- src/pages/ShuangDan/index.tsx
+++ src/pages/ShuangDan/index.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 
 const ShuangDan: FC = () => {
   const [isFixed, setIsFixed] = useState<boolean>(false);
+  const [active, setActive] = useState('effects');
 
   const secondRef = useRef<HTMLDivElement>(null);
 
@@ -11,6 +12,14 @@ const ShuangDan: FC = () => {
     if (secondRef.current) {
       const { top } = secondRef.current.getBoundingClientRect();
       setIsFixed(top <= 0);
+    }
+  }
+
+  const activate = (key: string) => {
+    setActive('key');
+    const el = document.querySelector(`[data-id="${key}"]`);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
@@ -28,8 +37,30 @@ const ShuangDan: FC = () => {
         first
       </div>
       <div className={styles.second} ref={secondRef}>
-        <div className={classNames(styles.header, { [styles.fixed]: isFixed })}>header</div>
-        second
+        <ul className={classNames(styles.header, { [styles.fixed]: isFixed })}>
+          <li className={classNames({ [styles.active]: active === 'effects' })} onClick={() => activate('effects')}>
+            Effects
+            <span className={styles.line}></span>
+          </li>
+          <li className={classNames({ [styles.active]: active === 'stickers' })} onClick={() => activate('stickers')}>
+            Stickers
+            <span className={styles.line}></span>
+          </li>
+          <li className={classNames({ [styles.active]: active === 'transitions' })} onClick={() => activate('transitions')}>
+            Transitions
+            <span className={styles.line}></span>
+          </li>
+          <li className={classNames({ [styles.active]: active === 'filters' })} onClick={() => activate('filters')}>
+            Filters
+            <span className={styles.line}></span>
+          </li>
+        </ul>
+        <div>
+          <section data-id='effects'>effects</section>
+          <section data-id='stickers'>stickers</section>
+          <section data-id='transitions'>transitions</section>
+          <section data-id='filters'>filters</section>
+        </div>
       </div>
       <div className={styles.third}>
         third
