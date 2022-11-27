bb79be9937cf0e101f6b269626a4844376dc93fb
haixiangyan <haixiang6123@gmail.com>
2022-11-27 10:52:31 +0800

    feat: 实现基线播放视频

--- src/pages/VideoFeeds/components/FeedList/index.tsx
+++ src/pages/VideoFeeds/components/FeedList/index.tsx
@@ -3,18 +3,19 @@ import {VideoData} from "../../constants/data";
 import styles from './styles.module.scss';
 
 interface Props {
+  id: string;
   list: VideoData[];
 }
 
 const Feed: FC<Props> = (props) => {
-  const { list } = props;
+  const { id, list } = props;
 
   return (
-    <div className={styles.wrapper}>
+    <div data-feed-list-id={id} className={styles.wrapper}>
       <ul className={styles.feedList}>
         {list.map(video => (
           <li key={video.id} data-video-id={`video-${video.id}`}>
-            <video src={video.src} />
+            <video loop muted src={video.src} />
           </li>
         ))}
       </ul>
--- src/pages/VideoFeeds/index.tsx
+++ src/pages/VideoFeeds/index.tsx
@@ -12,19 +12,35 @@ const VideoFeeds: FC = () => {
   const oldTopRef = useRef<number>(0);
 
   const onScroll: UIEventHandler<HTMLDivElement> = () => {
+    // 隐藏 NavBar
     if (contentRef.current) {
       const { top: newTop } = contentRef.current.getBoundingClientRect();
       const delta = newTop - oldTopRef.current;
 
       oldTopRef.current = newTop;
 
-      if (delta < 0) {
-        // 向上滑动
-        setNavBarHidden(true);
-      } else {
-        // 向下滑动
-        setNavBarHidden(false);
-      }
+      setNavBarHidden(delta < 0);
+    }
+
+    // 检查所有 FeedList
+    const midHeight = window.innerHeight / 2;
+
+    const feedListEls = Array.from(document.querySelectorAll('[data-feed-list-id]'));
+    const targetFeedListEl = feedListEls.find(el => {
+      const { top } = el.getBoundingClientRect();
+
+      return top <= midHeight && midHeight <= top + (16 * 2 + 240);
+    })
+
+    if (targetFeedListEl) {
+      const videoEls: HTMLVideoElement[] = Array.from(targetFeedListEl.querySelectorAll('video'));
+      console.log('video', videoEls);
+
+      // 前两个
+      videoEls[0]?.play();
+      videoEls[1]?.play();
+      // videoEls[0]?.setAttribute('autoplay', "true");
+      // videoEls[1]?.setAttribute('autoplay', "true");
     }
   }
 
@@ -35,13 +51,13 @@ const VideoFeeds: FC = () => {
       <div className={classNames(styles.wrapper, { [styles.hidden]: navBarHidden } )} onScroll={onScroll}>
         <div className={styles.content} ref={contentRef}>
           <h2>热门视频</h2>
-          <FeedList list={videoList1} />
+          <FeedList id="hot" list={videoList1} />
 
-          <h2>追番</h2>
-          <FeedList list={videoList1} />
+          <h2>生活</h2>
+          <FeedList id="life" list={videoList1} />
 
           <h2>影视</h2>
-          <FeedList list={videoList1} />
+          <FeedList id="movie" list={videoList1} />
         </div>
       </div>
     </div>
