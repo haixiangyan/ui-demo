60782de7c886dfdff4adf11c438d0e77e3ce6d4e
haixiangyan <haixiang6123@gmail.com>
2022-11-27 15:15:49 +0800

    feat: 优化滚动交互

--- src/pages/VideoFeeds/index.tsx
+++ src/pages/VideoFeeds/index.tsx
@@ -4,25 +4,17 @@ import styles from './styles.module.scss';
 import {dataSource} from "./constants/data";
 import FeedList from "./components/FeedList";
 import classNames from "classnames";
+import BannerImage from './assets/banner.png';
+import { debounce } from "lodash";
 
 const VideoFeeds: FC = () => {
   const [navBarHidden, setNavBarHidden] = useState<boolean>(false);
-  const [dynamic, setDynamic] = useState<string[]>([]);
 
+  const dynamicIdsRef = useRef<string[]>([])
   const contentRef = useRef<HTMLDivElement>(null);
   const oldTopRef = useRef<number>(0);
 
-  const onScroll: UIEventHandler<HTMLDivElement> = () => {
-    // 隐藏 NavBar
-    if (contentRef.current) {
-      const { top: newTop } = contentRef.current.getBoundingClientRect();
-      const delta = newTop - oldTopRef.current;
-
-      oldTopRef.current = newTop;
-
-      setNavBarHidden(delta < 0);
-    }
-
+  const autoPlayVideos = debounce(() => {
     // 检查所有 FeedList
     const midHeight = window.innerHeight / 2;
 
@@ -38,7 +30,7 @@ const VideoFeeds: FC = () => {
       const ids = dataSource[key].list.map(item => item.id).slice(0, 2);
 
       // 原来播放的内容要暂停
-      const stopIds = dynamic.filter(id => !ids.includes(id));
+      const stopIds = dynamicIdsRef.current.filter(id => !ids.includes(id));
 
       // 暂停所有
       stopIds.forEach((id) => {
@@ -48,22 +40,39 @@ const VideoFeeds: FC = () => {
       })
 
       // 需要播放的内容
-      const newPlayIds = ids.filter(id => !dynamic.includes(id));
+      const newPlayIds = ids.filter(id => !dynamicIdsRef.current.includes(id));
       newPlayIds.forEach(id => {
         const videoEl: HTMLVideoElement | null = document.querySelector(`[data-video-id="${id}"]`)
         videoEl?.play().then();
       })
 
       // 更新当前播放 Ids
-      setDynamic(ids);
+      dynamicIdsRef.current = ids;
     }
-  }
+  }, 300)
+
+  const onScroll: UIEventHandler<HTMLDivElement> = () => {
+    // 隐藏 NavBar
+    if (contentRef.current) {
+      const { top: newTop } = contentRef.current.getBoundingClientRect();
+      const delta = newTop - oldTopRef.current;
+
+      oldTopRef.current = newTop;
+
+      setNavBarHidden(delta < 0);
+    }
+
+    // 自动播放视频
+    autoPlayVideos();
+  };
 
   return (
     <div className={styles.container}>
       <NarBar hidden={navBarHidden} title="首页" />
 
       <div className={classNames(styles.wrapper, { [styles.hidden]: navBarHidden } )} onScroll={onScroll}>
+        <img className={styles.banner} src={BannerImage} alt="Banner"/>
+
         <div className={styles.content} ref={contentRef}>
           <h2>{dataSource.hot.title}</h2>
           <FeedList listId={dataSource.hot.id} list={dataSource.hot.list} />
